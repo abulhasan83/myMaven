cb060639038f84c3a69d46ab4c87088cb77e874c
abulhasan83 <julaihasan@gmail.com>
2019-12-09 15:47:01 +0530

    I have done my user story

--- src/test/java/sampleMaven/Abul.java
+++ src/test/java/sampleMaven/Abul.java
@@ -3,5 +3,7 @@ package sampleMaven;
 public class Abul {
 public static void main(String[] args) {
 	System.out.println("Abulhasan");
+	StringBuffer s1 = new StringBuffer("Julaiha");
+	System.out.println(s1);
 }
 }
